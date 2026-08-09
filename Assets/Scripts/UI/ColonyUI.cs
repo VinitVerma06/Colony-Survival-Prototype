a1ac62467d806dcbeed8b815ec233a07ba9a4359
VinitVerma06 <132371884+VinitVerma06@users.noreply.github.com>
2026-08-09 19:45:58 +0530

    wire simulation runner and ColonyUI to update visuals

---
+++ Assets/Scripts/UI/ColonyUI.cs
@@ -0,0 +1,46 @@
+using TMPro;
+using UnityEngine;
+using Simulation.Core;
+
+public class ColonyUI : MonoBehaviour {
+
+    [SerializeField] private TextMeshProUGUI dayCounterText;
+    [SerializeField] private TextMeshProUGUI foodReserveText;
+    [SerializeField] private TextMeshProUGUI waterReserveText;
+    [SerializeField] private TextMeshProUGUI foodDaysRemainingText;
+    [SerializeField] private TextMeshProUGUI waterDaysRemainingText;
+
+    private string dayCounterString = "Current Day : ";
+    private string foodReserveString = "Food Reserves : ";
+    private string waterReserveString = "Water Reserves : ";
+    private string foodDaysRemainingString = "Days Remaining for food : ";
+    private string waterDaysRemainingString = "Days Remaining for water : ";
+
+    private void OnEnable() {
+        SimulationRunner.OnDayAdvanced += SimulationRunner_OnDayAdvanced;
+    }
+
+    private void SimulationRunner_OnDayAdvanced(ColonySimulation simulation) {
+        UpdateVisual(simulation);
+    }
+
+    private void UpdateVisual(ColonySimulation simulation) {
+        
+        dayCounterText.text = dayCounterString + simulation.CurrentDay;
+        
+        foodReserveText.text = foodReserveString + simulation.FoodReserve;
+        
+        waterReserveText.text = waterReserveString + simulation.WaterReserve;
+        
+        foodDaysRemainingText.text = foodDaysRemainingString + 
+            simulation.DaysRemaining(simulation.FoodReserve, simulation.DailyFoodConsumption());
+        
+        waterDaysRemainingText.text = waterDaysRemainingString + 
+            simulation.DaysRemaining(simulation.WaterReserve, simulation.DailyWaterConsumption());
+    }
+
+
+    private void OnDestroy() {
+        SimulationRunner.OnDayAdvanced -= SimulationRunner_OnDayAdvanced;
+    }
+}
